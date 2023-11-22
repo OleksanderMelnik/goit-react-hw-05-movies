4bc6be04772343706872a164b69b5f72f9dbf416
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-11-22 17:30:22 +0200

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/HomePage";
+import Movies from "./pages/Movies/MoviesPage";
+import MoviesDetails from "./pages/MoviesDetails/MoviesDetailsPage";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/movies'>Movies</NavLink>
+            </li>
+            <li>
+              <NavLink to='movies/:movieId'>MoviesDetails</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MoviesDetails />} />
+      </Routes>
     </div>
   );
 };
